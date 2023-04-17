507440e7de4981b570bf6afba8f075379df1ed18
Bryan <blcolley@me.com>
2023-04-17 15:06:41 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState } from 'react'
+import './App.css'
+import './scss/main.scss'
+import NavBar from './components/NavBar'
+
+
+
+function App() {
+  return (
+  <NavBar/>
+    )
+}
+
+export default App
